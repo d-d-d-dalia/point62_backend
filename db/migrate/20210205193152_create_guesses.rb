a82f6a02761cbccc922a8efc06bccc35169f8981
Dalia <dalia.sawaya@flatironschool.com>
2021-02-22 14:40:06 -0500

    create guesses table

--- db/migrate/20210205193152_create_guesses.rb
+++ db/migrate/20210205193152_create_guesses.rb
@@ -1,7 +1,9 @@
 class CreateGuesses < ActiveRecord::Migration[6.0]
   def change
     create_table :guesses do |t|
-
+      t.string :player_name
+      t.integer :guess
+      t.float :kilometers
       t.timestamps
     end
   end
